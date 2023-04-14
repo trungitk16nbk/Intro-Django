50999b55f21b3a03768c3f994c40e163441860e0
Quoc Trung <buiquoctrung2002@gmail.com>
2023-04-15 00:22:56 +0700

    add decorator to detect error

--- StudentManage/myapp/models.py
+++ StudentManage/myapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
+
 class Student(models.Model):
     name = models.CharField(max_length=255)
     age = models.IntegerField() 
     gender = models.CharField(max_length=10)
     FlagDel =  models.BooleanField(default = False) 
-    
--- StudentManage/myapp/views.py
+++ StudentManage/myapp/views.py
@@ -1,140 +1,93 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.generic import View
-from django_http_method import HttpMethodMixin 
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 from django.db import OperationalError
 from .models import Student
 import json
 
-class StudentView(HttpMethodMixin, View):
+
+def myDecorator(func):
+    def swapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except OperationalError:
+            response = {
+                'status': 'fail',
+                'message': 'Database error'
+            }
+            return Response(response, status=status.HTTP_200_OK)
+        except Student.DoesNotExist:
+            response = {
+                'status': 'fail',
+                'message': 'Student not found!'
+            }
+            return Response(response, status=status.HTTP_200_OK)
+        except Exception as e:
+            response = {
+                'status': 'fail',
+                'message': 'Unknow error'
+            }
+            return Response(response, status=status.HTTP_200_OK)      
+    return  swapper      
+            
+            
+class StudentView(APIView):
+    parser_classes = [JSONParser]
+    
+    @myDecorator
     def post(self, request):
-        try:
-            student_data = json.loads(request.body)
-            student = Student(name = student_data['name'],  age = student_data['age'], gender = student_data['gender'])
-            student.save()
-            response = {
-                'status': 'success',
-                'message': 'Student added successfully!',
-                'student_id': student.id
-            }
-            return JsonResponse(response)
-        except OperationalError:
-            response = {
-                'status': 'fail',
-                'message': 'Database error'
-            }
-            return JsonResponse(response)
-        except Student.DoesNotExist:
-            response = {
-                'status': 'fail',
-                'message': 'Student not found!'
-            }
-            return JsonResponse(response)
-        except Exception as e:
-            response = {
-                'status': 'fail',
-                'message': 'Unknow error'
-            }
-            return JsonResponse(response)        
-        pass
+        student_data = json.loads(request.body)
+        student = Student(name = student_data['name'],  age = student_data['age'], gender = student_data['gender'])
+        student.save()
+        response = {
+            'status': 'success',
+            'message': 'Student added successfully!',
+            'student_id': student.id
+        }
+        return Response(response, status=status.HTTP_200_OK)
     
     
+    @myDecorator
     def get(self, request):
-        try: 
-            student_id = request.GET.get('id')
-            student = Student.objects.get(id = student_id, FlagDel = False)
+        student_id = request.GET.get('id')
+        student = Student.objects.get(id = student_id, FlagDel = False)
             
-            Student_data = {
-                'id' : student.id,
-                'name' : student.name,
-                'age' : student.age,
-                'gender' : student.gender
-            }
-            return JsonResponse(Student_data, safe=False)
-        except OperationalError:
-            response = {
-                'status': 'fail',
-                'message': 'Database error'
-            }
-            return JsonResponse(response)
-        
-        except Student.DoesNotExist:
-            response = {
-                'status': 'fail',
-                'message': 'Student not found!'
-            }
-            return JsonResponse(response)
-        except Exception as e:
-            response = {
-                'status': 'fail',
-                'message': 'Unknow error'
-            }
-            return JsonResponse(response)
-        pass
+        response = {
+            'id' : student.id,
+            'name' : student.name,
+            'age' : student.age,
+           'gender' : student.gender
+        }
+        return Response(response, status=status.HTTP_200_OK)
     
+    
+    @myDecorator
     def put(self, request):
-        try:
-            student_data = json.loads(request.body)
-            student_id = student_data['id']
-            student = Student.objects.get(id = student_id, FlagDel = False)
-            student.name = student_data['name']
-            student.age = student_data['age']
-            student.gender = student_data['gender']
-            student.FlagDel = False
-            student.save()
-            response = {
-            'status': 'success',
-            'message': 'Student information updated successfully!'
-            }
-            return JsonResponse(response)
-        except OperationalError:
-            response = {
-                'status': 'fail',
-                'message': 'Database error'
-            }
-            return JsonResponse(response)
-        except Student.DoesNotExist:
-            response = {
-                'status': 'fail',
-                'message': 'Student not found!'
-            }
-            return JsonResponse(response)
-        except Exception as e:
-            response = {
-                'status': 'fail',
-                'message': 'Unknow error'
-            }
-            return JsonResponse(response)        
-        pass
+        student_data = json.loads(request.body)
+        student_id = student_data['id']
+        student = Student.objects.get(id = student_id, FlagDel = False)
+        student.name = student_data['name']
+        student.age = student_data['age']
+        student.gender = student_data['gender']
+        student.FlagDel = False
+        student.save()
+        response = {
+        'status': 'success',
+        'message': 'Student information updated successfully!'
+        }
+        return Response(response, status=status.HTTP_200_OK)  
+    
+    
+    @myDecorator
     def delete(self, request):
-        try:
-            student_id = request.GET.get('id') 
-            student = Student.objects.get(id = student_id, FlagDel = False)
-            student.FlagDel = True
-            student.save()
-            response = {
-            'status': 'success',
-            'message': 'Delte student successfully!'
-            }
-            return JsonResponse(response)
+        student_id = request.GET.get('id') 
+        student = Student.objects.get(id = student_id, FlagDel = False)
+        student.FlagDel = True
+        student.save()
+        response = {
+        'status': 'success',
+        'message': 'Delte student successfully!'
+        }
+        return Response(response, status=status.HTTP_200_OK)
             
-        except OperationalError:
-            response = {
-                'status': 'fail',
-                'message': 'Database error'
-            }
-            return JsonResponse(response)
-        
-        except Student.DoesNotExist:
-            response = {
-                'status': 'fail',
-                'message': 'Student not found!'
-            }
-            return JsonResponse(response)
-        except Exception as e:
-            response = {
-                'status': 'fail',
-                'message': 'Unknow error'
-            }
-            return JsonResponse(response)
-        pass
